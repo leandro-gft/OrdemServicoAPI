7f000679d80a3e9241894c8b7679b02a255b492c
leandro-gft <lpsacchi@gmail.com>
2020-05-20 11:50:19 -0300

    Implementada busca por nome de cliente.

--- src/main/java/br/com/gft/osworks/domain/repository/ClienteRepository.java
+++ src/main/java/br/com/gft/osworks/domain/repository/ClienteRepository.java
@@ -1,9 +1,13 @@
 package br.com.gft.osworks.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gft.osworks.domain.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+	List<Cliente> findByNomeContaining(String nome);
+	
 }
