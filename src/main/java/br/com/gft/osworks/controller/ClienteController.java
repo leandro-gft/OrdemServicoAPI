3ef03e22e94867588b7727d3f0cd6669b62675cd
leandro-gft <lpsacchi@gmail.com>
2020-05-20 13:52:03 -0300

    Tratamento para cliente não encontrado

--- src/main/java/br/com/gft/osworks/controller/ClienteController.java
+++ src/main/java/br/com/gft/osworks/controller/ClienteController.java
@@ -3,30 +3,41 @@ package br.com.gft.osworks.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gft.osworks.domain.model.Cliente;
-import br.com.gft.osworks.domain.repository.ClienteRepository;
+import br.com.gft.osworks.domain.service.ClienteService;
 
 @RestController
-@RequestMapping(value="/api/clientes")
+@RequestMapping("/api/clientes")
 public class ClienteController {
-		
+
 	@Autowired
-	private ClienteRepository clienteRepository;
-	
+	private ClienteService clienteService;
+
 	@GetMapping
-	public List<Cliente> listar() {
-		return clienteRepository.findAll();
+	public ResponseEntity<List<Cliente>> listar() {
+		return ResponseEntity.ok(clienteService.listar());
 	}
-	
-	@GetMapping(value="/{nome}")
-	public List<Cliente> listar(@PathVariable String nome) {
-		return clienteRepository.findByNomeContaining(nome);
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+		Cliente cliente = clienteService.buscarPorId(id);
+		if (cliente != null) {
+			return ResponseEntity.ok(clienteService.buscarPorId(id));
+		} else {
+			return ResponseEntity.notFound().build();
+
+		}
 	}
-	
+
+//	@GetMapping("/{nome}")
+//	public ResponseEntity<List<Cliente>> buscarPorNome(@PathVariable String nome) {
+//		return ResponseEntity.ok(clienteService.buscarPorNome(nome));
+//	}
 
 }
---
+++ src/main/java/br/com/gft/osworks/domain/service/ClienteService.java
@@ -0,0 +1,29 @@
+package br.com.gft.osworks.domain.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.com.gft.osworks.domain.model.Cliente;
+import br.com.gft.osworks.domain.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public List<Cliente> listar() {
+		return clienteRepository.findAll();
+	}
+
+	public Cliente buscarPorId(@PathVariable Long id) {
+		return clienteRepository.findById(id).orElse(null);
+	}
+
+	public List<Cliente> buscarPorNome(String nome) {
+		return clienteRepository.findByNomeContaining(nome);
+	}
+}
