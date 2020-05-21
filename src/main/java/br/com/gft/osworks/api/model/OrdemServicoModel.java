79751569fa4a275aa10b1f92e98b2a1a472afd22
leandro-gft <lpsacchi@gmail.com>
2020-05-21 16:04:05 -0300

    Implementando DTO para cliente e ordem de serviço

---
+++ src/main/java/br/com/gft/osworks/api/model/OrdemServicoModel.java
@@ -0,0 +1,87 @@
+package br.com.gft.osworks.api.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import br.com.gft.osworks.domain.model.StatusOrdemServico;
+
+@JsonInclude(Include.NON_NULL)
+public class OrdemServicoModel {
+	
+	private Long id;
+	private String descricao;
+	private BigDecimal preco;
+	private StatusOrdemServico status;
+	private OffsetDateTime dataAbertura = OffsetDateTime.now();
+	private OffsetDateTime dataFinalizacao;
+	private ClienteResumoModel cliente;
+		
+	public ClienteResumoModel getCliente() {
+		return cliente;
+	}
+	public void setCliente(ClienteResumoModel cliente) {
+		this.cliente = cliente;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	public StatusOrdemServico getStatus() {
+		return status;
+	}
+	public void setStatus(StatusOrdemServico status) {
+		this.status = status;
+	}
+	public OffsetDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+	public void setDataAbertura(OffsetDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	public OffsetDateTime getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdemServicoModel other = (OrdemServicoModel) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+}
--- src/main/java/br/com/gft/osworks/resource/OrdemServicoResource.java
+++ src/main/java/br/com/gft/osworks/resource/OrdemServicoResource.java
@@ -1,9 +1,11 @@
 package br.com.gft.osworks.resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.gft.osworks.api.model.OrdemServicoModel;
 import br.com.gft.osworks.domain.model.OrdemServico;
 import br.com.gft.osworks.domain.service.OrdemServicoService;
 
@@ -24,28 +27,38 @@ public class OrdemServicoResource {
 	@Autowired
 	private OrdemServicoService osService;
 
+	@Autowired
+	private ModelMapper mapper;
+
 	@PostMapping
-	public ResponseEntity<OrdemServico> criarOS(@RequestBody @Valid OrdemServico ordemServico) {
+	public ResponseEntity<OrdemServicoModel> criarOS(@RequestBody @Valid OrdemServico ordemServico) {
 		osService.criar(ordemServico);
 		return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(ordemServico.getId()).toUri()).body(ordemServico);
+				.buildAndExpand(ordemServico.getId()).toUri()).body(toModel(ordemServico));
 	}
 
 	@GetMapping
-	public ResponseEntity<List<OrdemServico>> buscarOrdens() {
-		return ResponseEntity.ok(osService.listar());
+	public ResponseEntity<List<OrdemServicoModel>> buscarOrdens() {
+		return ResponseEntity.ok(toModelList(osService.listar()));
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<OrdemServico> buscarOrdensPorId(@PathVariable Long id) {
+	public ResponseEntity<OrdemServicoModel> buscarOrdensPorId(@PathVariable Long id) {
 		OrdemServico os = osService.listarPorId(id);
 		if (os != null) {
-			return ResponseEntity.ok(os);
+			return ResponseEntity.ok(toModel(os));
 		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
-	
-	
+
+	public OrdemServicoModel toModel(OrdemServico os) {
+		return mapper.map(os, OrdemServicoModel.class);
+	}
+
+	public List<OrdemServicoModel> toModelList(List<OrdemServico> os) {
+		return os.stream().map(ordemServico -> toModel(ordemServico)).collect(Collectors.toList());
+	}
+
 }
+ 
